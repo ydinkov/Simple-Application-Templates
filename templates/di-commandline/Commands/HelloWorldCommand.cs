aa89455514af4bf7864d8febb43758b3a597606b
Yasen <ydinkov@gmail.com>
2022-02-22 22:52:47 +0100

    Initial Commit

---
+++ templates/di-commandline/Commands/HelloWorldCommand.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+
+
+namespace DependenctInjectedCommandLineApplication.Commands
+{
+    public class HelloWorldCommand : Command
+    {
+        private readonly ILogger logger;
+
+        public HelloWorldCommand(ILogger<HelloWorldCommand> logger) 
+            : base(name: "hello", description: "Greets the user")
+        {
+            this.logger = logger;
+
+            AddOption(new Option<string>(new[] { "--name", "-n" }, getDefaultValue: () => "Mr. User")
+            {
+                Description = "The user's name. Used to personalise message",
+                IsRequired = false
+            });
+
+
+            Handler =  CommandHandler.Create<string>((string name) => 
+            {
+             
+                this.logger.LogInformation($"Hello {name}!");
+            });
+        }       
+    }
+}
---
+++ templates/di-commandline/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using DependenctInjectedCommandLineApplication.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var services = new ServiceCollection();
+
+services.AddLogging(x => x.AddConsole()) ;
+services.AddSingleton<Command, HelloWorldCommand>();
+
+var serviceProvider = services.BuildServiceProvider();
+var rootCommand = new RootCommand();
+foreach (Command command in serviceProvider.GetServices<Command>())
+    rootCommand.AddCommand(command);
+
+var commandLineBuilder = new CommandLineBuilder(rootCommand);
+var parser = commandLineBuilder.UseDefaults().Build();
+
+return parser.Invoke(args);
